7b3e139ffc46b9b13a53b709a9a7890622df031c
Eli Schultz <eli.k.schultz@gmail.com>
2020-03-06 23:22:43 -0500

    Modified bug in choosing edge to contract

--- karger_min_cut.py
+++ karger_min_cut.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import copy
+import time
 
 # Read in file
 with open('kargerMinCut.txt', 'r') as f:
@@ -8,54 +10,60 @@ with open('kargerMinCut.txt', 'r') as f:
 
 graph = graph.split('\n')[:-1]
 
-# Create list to track number of crossing edges for each pass of algorithm
-crossing_edges = []
+# Represent adjacency list as dictionary of nodes, where each entry consists of another
+# dictionary with keys corresponding to adjacent nodes and values corresponding to 
+# number of edges connecting the two
+graph_dict = {}
+for vertex in graph:
+    vertex = vertex.split('\t')[:-1]
+    graph_dict[vertex[0]] = {}
+    for connected_vertex in vertex[1:]:
+        graph_dict[vertex[0]][connected_vertex] = 1
 
-# Run algorithm 1000 times
-for i in range(1000): 
-    # Represent adjacency list as dictionary of nodes, where each entry consists of another
-    # dictionary with keys corresponding to adjacent nodes and values corresponding to 
-    # number of edges connecting the two
-    graph_dict = {}
-    for vertex in graph:
-        vertex = vertex.split('\t')[:-1]
-        graph_dict[vertex[0]] = {}
-        for connected_vertex in vertex[1:]:
-            graph_dict[vertex[0]][connected_vertex] = 1
-    
-    final_node_1= ''
-    final_node_2 = ''
-    while len(graph_dict.keys()) > 2: 
+# Create list to track number of crossing edges for each pass of algorithm
+crossing_edges = 200*199/2
+
+t1 = time.time()
+# Run algorithm 40000 times
+for i in range(40000):
+    graph_dict_copy = copy.deepcopy(graph_dict)
+    while len(graph_dict_copy.keys()) > 2: 
         # Choose two nodes to combine at random
-        to_combine = np.random.choice(list(graph_dict.keys()), size = 2, replace = False)
+        to_combine_0 = np.random.choice(list(graph_dict_copy.keys()), size = 1)[0]
+        to_combine_1 = np.random.choice(list(graph_dict_copy[to_combine_0].keys()), 
+                                        size = 1)[0]
         
         # Add new key to adjacency list representing merged node and get adjacent nodes/counts of 
         # connecting edges
-        new_node = to_combine[0] + ", " + to_combine[1]
-        graph_dict[new_node] = {}
-        for adj_node in graph_dict[to_combine[0]].keys():
-            if adj_node != to_combine[1]:
-                graph_dict[new_node][adj_node] = graph_dict[to_combine[0]][adj_node]
-        for adj_node in graph_dict[to_combine[1]].keys():
-            if adj_node != to_combine[0]:
-                graph_dict[new_node][adj_node] = graph_dict[new_node].get(adj_node, 0) + \
-                    graph_dict[to_combine[1]][adj_node]
+        new_node = to_combine_0 + ", " + to_combine_1
+        graph_dict_copy[new_node] = graph_dict_copy[to_combine_0]
+        try:
+            del graph_dict_copy[new_node][to_combine_1]
+        except KeyError:
+            pass
+        
+        for adj_node in graph_dict_copy[to_combine_1].keys():
+            if adj_node != to_combine_0:
+                graph_dict_copy[new_node][adj_node] = graph_dict_copy[new_node].get(adj_node, 0) + \
+                    graph_dict_copy[to_combine_1][adj_node]
     
         # Replace all references to the two nodes that were merged with references to the new node
-        for node in graph_dict.keys():
-            if to_combine[0] in graph_dict[node].keys():
-                graph_dict[node][new_node] = graph_dict[node][to_combine[0]]
-                del graph_dict[node][to_combine[0]]
+        for node in graph_dict_copy.keys():
+            if to_combine_0 in graph_dict_copy[node].keys():
+                graph_dict_copy[node][new_node] = graph_dict_copy[node][to_combine_0]
+                del graph_dict_copy[node][to_combine_0]
                 
-            if to_combine[1] in graph_dict[node].keys():
-                graph_dict[node][new_node] = graph_dict[node].get(new_node, 0) + \
-                    graph_dict[node][to_combine[1]]
-                del graph_dict[node][to_combine[1]]
+            if to_combine_1 in graph_dict_copy[node].keys():
+                graph_dict_copy[node][new_node] = graph_dict_copy[node].get(new_node, 0) + \
+                    graph_dict_copy[node][to_combine_1]
+                del graph_dict_copy[node][to_combine_1]
                 
-        del graph_dict[to_combine[0]]
-        del graph_dict[to_combine[1]]
+        del graph_dict_copy[to_combine_0]
+        del graph_dict_copy[to_combine_1]
         
         final_node_1 = new_node
-        final_node_2 = list(graph_dict[new_node].keys())[0]
+        final_node_2 = list(graph_dict_copy[new_node].keys())[0]
 
-    crossing_edges.append(graph_dict[final_node_1][final_node_2])
+    if graph_dict_copy[final_node_1][final_node_2] < crossing_edges:
+        crossing_edges = graph_dict_copy[final_node_1][final_node_2]
+print(time.time() - t1)
